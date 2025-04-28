c7e71dee4972e3035d2f0a4c89203d63cb6c0f27
omidshabgard <omidsangar@gmail.com>
2025-04-28 13:26:24 -0400

    LoginForm

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { LoginForm } from "./LoginForm";
+import React from 'react';
+import { LoginForm } from './LoginForm';
+import './LoginForm.css';
 
 export const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  return (
-    <div className="w-96 h-96 bg-white p-4">
-      {!isLoggedIn ? (
-        <LoginForm onLogin={() => setIsLoggedIn(true)} />
-      ) : (
-        <div>Chat interface will go here</div>
-      )}
-    </div>
-  );
+	return (
+		<div className='w-96 h-96 bg-white p-4'>
+			{!isLoggedIn ? (
+				<LoginForm onLogin={() => setIsLoggedIn(true)} />
+			) : (
+				<div className='text-center'>
+					<h1 className='text-2xl font-bold'>
+						Welcome to the Chat App!
+					</h1>
+					<p className='mt-4'>You are now logged in.</p>
+				</div>
+			)}
+		</div>
+	);
 };
