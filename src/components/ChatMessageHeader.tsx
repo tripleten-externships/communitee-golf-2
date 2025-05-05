e929105317fb7243291f6f024906b7b5c6955afb
Shelton Brockett <sheltonbrockett@gmail.com>
2025-05-04 21:51:50 -0400

    feat(ChatMessageHeader):update ChatMessageHeader with useEffect

--- src/components/ChatMessageHeader.tsx
+++ src/components/ChatMessageHeader.tsx
@@ -2,20 +2,39 @@
 import placeholder from "../assets/placeholder.png";
 import backbutton from "../assets/backbtn.svg";
 // import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ChatMessageHeader = () => {
-  // const [user, setUser] = useState({ name: "", avatar: "" });
+  const [user, setUser] = useState({ name: "", avatar: "" });
   // const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => {
+        setUser({
+          name: data.name,
+          avatar: data.avatarUrl,
+        });
+      })
+      .catch((err) => {
+        console.error("Failed to load user", err);
+        setUser({
+          name: "Unknown User",
+          avatar: "",
+        });
+      });
+  }, []);
 
   return (
     <div>
       <div className="flex justify-between">
         {" "}
         <img
-          className=""
+          className="cursor-pointer"
           alt="back-button"
           src={backbutton}
-          // onClick={() => navigate(-1)}
+          // onClick={}
         ></img>
         <img
           className="w-9 h-9 rounded-full mx-auto"
