b6cf856a5c7611409079dc35f2457e7e7050019f
RosaYi <rosayihuilin@gmail.com>
2025-05-07 18:46:21 -0400

    style location dropdown menu

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -10,12 +10,12 @@ export const DropdownMenu: React.FC = () => {
 	};
 
 	return (
-		<div className='relative inline-block text-left'>
-			<Menu as='div'>
-				<div>location</div>
+		<div className='w-[336px] h-[595px] bg-white rounded-md pt-5 pb-5 px-4 relative inline-block text-left'>
+			<Menu as='div' className="relative w-full">
+				<div className='text-sm text-gray-600 mt-8 mb-1'>location</div>
 				<div>
-					<MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-8 py-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
-						{selectedCourse || 'Select a Golf Course'}
+					<MenuButton className='inline-flex w-full justify-between rounded-[12px] bg-white px-4 py-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 outline outline-1 outline-black '>
+						{selectedCourse || 'Gilory Golf Course'}
 						<ChevronDownIcon
 							aria-hidden='true'
 							className='-mr-1 h-5 w-5 text-gray-400'
@@ -23,7 +23,7 @@ export const DropdownMenu: React.FC = () => {
 					</MenuButton>
 				</div>
 
-				<MenuItems className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
+				<MenuItems className='absolute right-0 z-10 mt-2 w-full origin-top-right rounded-[12px] bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
 					<div className='py-1'>
 						{[
 							'Golf Course One',
