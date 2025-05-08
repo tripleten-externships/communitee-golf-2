998de8d8f2b1319941795b4730e92848e47eabe1
Shelton Brockett <sheltonbrockett@gmail.com>
2025-05-07 20:45:07 -0400

    feat(MessageTab):update MessageTab state management

--- src/components/MessageTab.tsx
+++ src/components/MessageTab.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 export const MessageTab = () => {
-  const [messageCount, setMessageCount] = useState(0);
+  const [unreadCount, setunreadCount] = useState(0);
 
   useEffect(() => {
     fetch("api")
       .then((res) => res.json())
       // move into api file when working with backend
       .then((data) => {
-        setMessageCount(data.length);
+        setunreadCount(data.length);
       })
       .catch(console.error);
   }, []);
@@ -17,7 +17,7 @@ export const MessageTab = () => {
     <div>
       <div className="p-[16px]">
         {" "}
-        <h2 className="m-0 p-4 text-center ">Messages ({messageCount})</h2>
+        <h2 className="m-0 p-4 text-center ">Messages ({unreadCount})</h2>
         {/* {message list} */}
       </div>
     </div>
