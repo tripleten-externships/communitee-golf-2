0283b2da90fcb1ce74560bfa4a52ce5632f6bcac
omidshabgard <omidsangar@gmail.com>
2025-05-16 12:21:53 -0400

    feat: reimplement loginform with tailwind and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,18 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: true,
-  },
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
 };
-
 export default config;
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { LoginForm } from "./LoginForm";
-import { ChatMessageHeader } from "./ChatMessageHeader";
-import { useState } from "react";
-import { User } from "../types";
+import React from 'react';
+import { LoginForm } from './LoginForm';
+import { ChatMessageHeader } from './ChatMessageHeader';
+import { useState } from 'react';
+import { User } from '../types';
 
 export const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [user] = useState<User>({ name: "", avatar: "" });
+	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+	const [user] = useState<User>({ name: '', avatar: '' });
 
-  return (
-    <div className="w-96 h-96 bg-white p-4">
-      {!isLoggedIn ? (
-        <LoginForm onLogin={() => setIsLoggedIn(true)} />
-      ) : (
-        <ChatMessageHeader user={user} />
-      )}
-    </div>
-  );
+	return (
+		<div className='w-96 h-96 bg-white p-4'>
+			{!isLoggedIn ? (
+				<LoginForm onLogin={() => setIsLoggedIn(true)} />
+			) : (
+				<ChatMessageHeader user={user} />
+			)}
+		</div>
+	);
 };
