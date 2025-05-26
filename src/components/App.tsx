73c0c8f0938aeb0dc5d925ac9337f0d44303048b
RosaYi <rosayihuilin@gmail.com>
2025-05-26 19:36:59 -0400

    fix: merge

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import { LoginForm } from "./LoginForm";
 import MessageAreaStream from "./MessageAreaStream";
 
 export const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [unreadCount] = useState(0);
 
   return (
     <div className="w-96 h-[595px] bg-white p-4">
