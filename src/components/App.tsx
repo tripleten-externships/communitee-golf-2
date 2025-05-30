70dbe61dd10a5e36cf8f451c0c26d5fc6db65486
omidshabgard <omidsangar@gmail.com>
2025-05-30 09:29:37 -0400

    dropdown displays after login

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { LoginForm } from "./LoginForm";
+import React from 'react';
+import { LoginForm } from './LoginForm';
+import { DropdownMenu } from './DropdownMenu'; // make sure this path is correct
 
 export const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  return (
-    <div className="w-96 h-96 bg-white p-4">
-      {!isLoggedIn ? (
-        <LoginForm onLogin={() => setIsLoggedIn(true)} />
-      ) : (
-        <div>Chat interface will go here</div>
-      )}
-    </div>
-  );
+	return (
+		<div className='w-96 h-96 bg-white p-4'>
+			{!isLoggedIn ? (
+				<LoginForm onLogin={() => setIsLoggedIn(true)} />
+			) : (
+				<>
+					<DropdownMenu />
+					<div></div>
+				</>
+			)}
+		</div>
+	);
 };
